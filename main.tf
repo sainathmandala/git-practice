5beb724c81e902cc325221501bab3c3a9ab05180
Ubuntu <ubuntu@ip-172-31-27-14.ec2.internal>
2025-05-27 10:16:10 +0000

    Formatted Terraform files

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "aws_s3_bucket" "my_bucket" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
 
   tags = {
     Name = "EC2FromTerraform"
